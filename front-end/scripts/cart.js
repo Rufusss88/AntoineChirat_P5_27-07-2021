ab1bfcff1dd9a2be44a5e92ecd980586bd8401da
Rufusss88 <ant.chirat@gmail.com>
2021-09-03 21:22:53 +0200

    premier essai sur la page panier

--- front-end/scripts/cart.js
+++ front-end/scripts/cart.js
@@ -1,3 +1,16 @@
 
+let arrayProductsInCart = JSON.parse(localStorage.getItem("product"));
 
+console.log(arrayProductsInCart);
 
+//AFFICHAGE DES PRODUITS DU PANIER//
+
+//SELECTION DE LA CLASSE OU ON INJECTE LE CODE HTML//
+
+//PANIER VIDE//
+if (arrayProductsInCart === null){
+    console.log("vide");
+}
+else{
+    console.log("je suis plein");
+}
